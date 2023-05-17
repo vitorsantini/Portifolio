5f94902e8c0d10558d543b853c2ef35c25b40d68
vitorsantini <santini.vitor@hotmail.com>
2023-05-17 02:43:21 -0300

    Tentantiva frustada de implementar um toggle para o lightmode

--- js/swiper.js
+++ js/swiper.js
@@ -1,6 +1,6 @@
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 2,
-    spaceBetween: 30,
+    slidesPerView: 1,
+    spaceBetween: 36,
     loop: false,
     pagination: {
       el: ".swiper-pagination",
@@ -10,4 +10,16 @@ var swiper = new Swiper(".mySwiper", {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
+    breakpoints: {
+      1024: {
+        slidesPerView: 2,
+        spaceBetween: 64,
+      }
+
+    },
+
+
   });
+
+swiper.style.marginLeft = '160px !important';
+swiper.style.marginRight = '160px !important'; 
