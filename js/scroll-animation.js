99a1928e143b5917854ea9ed0f27f4e8ae5a7af0
vitorsantini <santini.vitor@hotmail.com>
2023-05-29 02:39:35 -0300

    Adicionando Pojeto React e Push to top button

--- js/scroll-animation.js
+++ js/scroll-animation.js
@@ -10,3 +10,23 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
+
+
+const goTopButton = document.querySelector('.go-to-top');
+
+window.addEventListener('scroll', checkHeight)
+
+function checkHeight(){
+    if(window.scrollY > 200){
+        goTopButton.style.display = 'flex'
+    } else {
+        goTopButton.style.display = 'none'
+    }
+}
+
+goTopButton.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behaveior: 'smooth'
+    })
+})
